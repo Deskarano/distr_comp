1773c042d7e8b54c9b97c94ecc8133c767191058
Gregor Haas <gregorhaas1997@gmail.com>
2018-11-16 19:35:56 -0500

    made it easier to mess up command args

--- src/peer/client/TextClient.java
+++ src/peer/client/TextClient.java
@@ -31,107 +31,114 @@ public class TextClient extends AbstractClientPeer
         Scanner input = new Scanner(System.in);
         Socket server = null;
 
-        while(true)
+        try
         {
-            System.out.print(HEADER + " >>> ");
-            String read = input.nextLine();
-
-            String[] command = read.split(" ");
-
-            switch (command[0])
+            while (true)
             {
-                case COMMAND_CONNECT: // args: [from]
-                    server = connectToServer(command[1]);
-                    break;
+                System.out.print(HEADER + " >>> ");
+                String read = input.nextLine();
 
-                case COMMAND_DISCONNECT:
-                    try
-                    {
-                        if(server != null)
+                String[] command = read.split(" ");
+
+                switch (command[0])
+                {
+                    case COMMAND_CONNECT: // args: [from]
+                        server = connectToServer(command[1]);
+                        break;
+
+                    case COMMAND_DISCONNECT:
+                        try
                         {
-                            server.close();
+                            if (server != null)
+                            {
+                                server.close();
+                            }
                         }
-                    }
-                    catch (IOException e)
-                    {
-                        e.printStackTrace(System.out);
-                    }
-
-                    break;
-
-                case COMMAND_SEND_FILE: // args: [from]
-                    File f = new File(command[1]);
-
-                    try
-                    {
-                        BufferedReader serverReader = new BufferedReader(new InputStreamReader(server.getInputStream()));
-                        PrintWriter serverWriter = new PrintWriter(server.getOutputStream(), true);
-
-                        BufferedReader fileReader = new BufferedReader(new FileReader(f));
-
-                        String line;
-                        while((line = fileReader.readLine()) != null)
+                        catch (IOException e)
                         {
-                            System.out.println(HEADER + ": sending line " + line);
-                            serverWriter.println(line);
+                            e.printStackTrace(System.out);
+                        }
+
+                        break;
+
+                    case COMMAND_SEND_FILE: // args: [from]
+                        File f = new File(command[1]);
+
+                        try
+                        {
+                            BufferedReader serverReader = new BufferedReader(new InputStreamReader(server.getInputStream()));
+                            PrintWriter serverWriter = new PrintWriter(server.getOutputStream(), true);
+
+                            BufferedReader fileReader = new BufferedReader(new FileReader(f));
+
+                            String line;
+                            while ((line = fileReader.readLine()) != null)
+                            {
+                                System.out.println(HEADER + ": sending line " + line);
+                                serverWriter.println(line);
+
+                                String response = serverReader.readLine();
+                                System.out.println(HEADER + ": received response " + response);
+                            }
+
+                            fileReader.close();
+                            serverWriter.close();
+                            serverReader.close();
+                        }
+                        catch (FileNotFoundException e)
+                        {
+                            e.printStackTrace(System.out);
+                        }
+                        catch (IOException e)
+                        {
+                            e.printStackTrace(System.out);
+                        }
+                        break;
+
+                    case COMMAND_SEND_STR:
+                        String str = command[2];
+
+                        try
+                        {
+                            BufferedReader serverReader = new BufferedReader(new InputStreamReader(server.getInputStream()));
+                            PrintWriter serverWriter = new PrintWriter(server.getOutputStream(), true);
+
+                            System.out.println(HEADER + ": sending message " + str);
+                            serverWriter.println(str);
 
                             String response = serverReader.readLine();
                             System.out.println(HEADER + ": received response " + response);
+
+                            serverReader.close();
+                            serverWriter.close();
                         }
-
-                        fileReader.close();
-                        serverWriter.close();
-                        serverReader.close();
-                    }
-                    catch (FileNotFoundException e)
-                    {
-                        e.printStackTrace(System.out);
-                    }
-                    catch (IOException e)
-                    {
-                        e.printStackTrace(System.out);
-                    }
-                    break;
-
-                case COMMAND_SEND_STR:
-                    String str = command[2];
-
-                    try
-                    {
-                        BufferedReader serverReader = new BufferedReader(new InputStreamReader(server.getInputStream()));
-                        PrintWriter serverWriter = new PrintWriter(server.getOutputStream(), true);
-
-                        System.out.println(HEADER + ": sending message " + str);
-                        serverWriter.println(str);
-
-                        String response = serverReader.readLine();
-                        System.out.println(HEADER + ": received response " + response);
-
-                        serverReader.close();
-                        serverWriter.close();
-                    }
-                    catch (IOException e)
-                    {
-                        e.printStackTrace(System.out);
-                    }
-
-                    break;
-
-                case COMMAND_EXIT:
-                    try
-                    {
-                        if(server != null)
+                        catch (IOException e)
                         {
-                            server.close();
+                            e.printStackTrace(System.out);
                         }
-                    }
-                    catch (IOException e)
-                    {
-                        e.printStackTrace(System.out);
-                    }
 
-                    return;
+                        break;
+
+                    case COMMAND_EXIT:
+                        try
+                        {
+                            if (server != null)
+                            {
+                                server.close();
+                            }
+                        }
+                        catch (IOException e)
+                        {
+                            e.printStackTrace(System.out);
+                        }
+
+                        return;
+                }
             }
+        }
+        catch(ArrayIndexOutOfBoundsException e)
+        {
+            e.printStackTrace(System.out);
         }
     }
 }
